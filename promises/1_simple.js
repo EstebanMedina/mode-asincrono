0757148cc04eb4e38fbb1e74ac7d117455b178d4
Your Name <you@example.com>
2022-10-12 10:00:21 -0500

    mode_asincronico

---
+++ promises/1_simple.js
@@ -0,0 +1,45 @@
+//dependencia a XMLHttpRequest
+const http = require('xmlhttprequest').XMLHttpRequest
+
+const url = "https://pokeapi.co/api/v2/type"
+
+//funcion para conectar a una API publica
+function get_data(endpoint ){
+    let promise = new Promise(function(resolve , rejected){
+    //1.Crear el objeto de conexion
+    const h = new http()
+    //2.Abrir una conexion a la API
+    h.open('GET' , endpoint)
+    //3.Enviar la request definida
+     h.send()
+     //4. una vez, recibida la respuesta,
+     //  tratar la info
+     h.onload = function(){
+        if(h.status === 200){
+
+        }else{
+            rejected(h.status)
+        }
+        resolve(h.responseText)
+    }
+})
+    return promise
+}
+
+function exito(data){
+    const tipos = JSON.parse(data).results
+    //recorrer el arreglode tipos
+    tipos.forEach(function(element) {
+        console.log(`Tipo: ${element.name}`)
+        console.log(`---------------------`)
+    });
+}
+
+function fallo(status){
+    console.log(status)
+}
+
+     //invocar get data
+     get_data(url).then(function(data){
+        exito(data)
+     })
